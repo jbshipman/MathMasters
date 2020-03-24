c1c9f3ff08b09c4c5b397543398435cdd49884d3
mattjagiello <mjagiello@gmail.com>
2020-03-24 12:36:42 -0500

    Finished skeleton of user profile

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -37,12 +37,12 @@ function userLogin(users) {
     userList.appendChild(userLi);
     userLi.addEventListener('click', (e) => {
       e.preventDefault();
-      console.log(`Clicked ${user.name} profile, id:${user.id}`);
       renderUserProfile(user);
     });
   });
 }
 
+// Display User Profile
 function renderUserProfile(user) {
   console.log(`${user.name}, ${user.id}`);
   const mainDiv = document.getElementById('main');
@@ -55,9 +55,43 @@ function renderUserProfile(user) {
 
   // Take New Test
   newTestInstructions = document.createElement('p');
-  newTestInstructions.innerText = 'Would you like a new test?';
+  newTestInstructions.innerText = 'Would you like to take a new test?';
   mainDiv.append(newTestInstructions);
-  
 
+  // Test Button - Easy
+  const easyTest = document.createElement('button');
+  easyTest.innerText = 'Take Easy Test';
+  easyTest.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Clicked easy test button');
+    // TODO: Easy Test Function
+  });
 
+  // Test Button - Hard
+  const hardTest = document.createElement('button');
+  hardTest.innerText = 'Take Hard Test';
+  hardTest.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Clicked hard test button');
+    // TODO: Hard Test Function
+  });
+  mainDiv.append(easyTest);
+  mainDiv.append(hardTest);
+
+  // Display Test Results
+  const userResultDiv = document.createElement('div');
+  mainDiv.append(userResultDiv);
+  const resultsText = document.createElement('p');
+  resultsText.innerText = 'Your test results and scores:';
+  userResultDiv.append(resultsText);
+  // TODO: Display results function
+  // Option to retake certain test?
+
+  // Display Questions for Review
+  const userReviewDiv = document.createElement('div');
+  mainDiv.append(userReviewDiv);
+  const reviewText = document.createElement('p');
+  reviewText.innerText = 'Your marked questions for review:';
+  userReviewDiv.append(reviewText);
+  // TODO: Display reviewed questions
 }
